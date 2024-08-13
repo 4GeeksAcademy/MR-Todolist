b2820abead4f0ba62d745c66d44252eac91d31ca
MicaelaRojas <67761871+MicaelaRojas@users.noreply.github.com>
2024-08-13 21:34:01 +0000

    Todolist finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,73 @@
-import React from "react";
-
-//include images into your bundle
+import React, { useState } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
 
+const Home = () => {
+	const [tasks, setTasks] = useState([]);
+	const [inputValue, setInputValue] = useState('');
+  
+	const handleChange = (event) => {
+	  setInputValue(event.target.value);
+	};
+  
+	const handleKeyPress = (event) => {
+	  if (event.key === 'Enter') {
+		validateInput();
+	  }
+	};
+  
+	const validateInput = () => {
+	  if (inputValue.trim() === "") {
+		alert("La tarea no puede estar vacía");
+	  } else {
+		setTasks([...tasks, inputValue.trim()]);
+		setInputValue('');
+	  }
+	};
+  
+	const handleDelete = (index) => {
+	  setTasks(tasks.filter((_, i) => i !== index));
+	};
+  
+	return (
+	  <div className="text-center">
+		<h1 class="display-1">TODOlist</h1>
+		<div className="card mt-4" style={{ maxWidth: '600px', margin: '0 auto' }}>
+		  <div className="card-body">
+			<input
+			  type="text"
+			  value={inputValue}
+			  onChange={handleChange}
+			  onKeyDown={handleKeyPress}
+			  placeholder="Añadir tarea..."
+			  className="form-control mb-3"
+			/>
+		
+			<ul className="list-group list-group-flush">
+			  {tasks.length === 0 ? (
+				<li className="list-group-item">No hay tareas, añadir tareas</li>
+			  ) : (
+				tasks.map((task, index) => (
+				  <li
+					key={index}
+					className="list-group-item d-flex justify-content-between align-items-center task-item"
+				  >
+					{task}
+					<button
+					  className="delete-button btn btn-danger btn-sm"
+					  onClick={() => handleDelete(index)}
+					>
+					<i class="fa-solid fa-trash"></i>
+					</button>
+				  </li>
+				))
+			  )}
+			</ul>
+		  </div>
+		</div>
+	  </div>
+	);
+  };
+  
+  
 export default Home;
